9afcc5b5bdbabdf9b38a12e797681078aeb3abcc
arcturus-9952 <35000@ocvts.org>
2025-01-24 17:34:08 +0000

    rps 1234

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let playerScore = 0;
 let computerScore = 0;
 let gameLead = '';
 
+const choices = ['rock', 'paper', 'scissors'];
+
 const timer = {
   timeLeft: 2,
   timerInterval: null,
@@ -27,7 +29,7 @@ const timer = {
           }
 
           this.timeLeft--;
-      }, 500); // Decreases the timer every 350 milliseconds
+      }, 500); // Decreases the timer every 500 milliseconds
   },
 
   reset: function () {
@@ -37,33 +39,11 @@ const timer = {
   }
 };
 
-const handleInputChoice = () => {
-  const userInput = document.getElementById('userInput').value;
-  document.getElementById('userInput').value = '';
-  getUserChoice(userInput);
-}
-
-  const getUserChoice = (userInput) => {
-
-    const validChoices = ['rock', 'paper', 'scissors'];
-    let choice = userInput.toLowerCase();
-
-    if (!validChoices.includes(choice)) {
-      alert('Wrong choice. Please choose rock, paper, or scissors...')
-      return;
-    }
-
+  const getUserChoice = (choice) => {
     userChoice = choice.charAt(0).toUpperCase() + choice.slice(1);
 
-    // userChoice = userInput;
-    if (userChoice === 'Rock') {
-      document.getElementById('userChoice').className = 'rockChar';
-    } else if (userChoice === 'Paper') {
-      document.getElementById('userChoice').className = '';
-    } else if (userChoice === 'Scissors') {
-      document.getElementById('userChoice').className = 'scissorsChar';
-    }
-    document.getElementById('userChoice').innerHTML = ('Player selected: ' + userChoice);
+    document.getElementById('userChoice').innerHTML = 'Player selected: ' + userChoice;
+    document.getElementById('userChoice').className = `${choice}Char`;
     clearInterval(timer.timerInterval);
     
     disableButtons();
@@ -72,18 +52,10 @@ const handleInputChoice = () => {
 
 
 const getComputerChoice = () => {
-    let randomNumber = Math.floor(Math.random() * 3); 
-    if (randomNumber === 0) {
-      computerChoice = 'Rock';
-      document.getElementById('computerChoice').className = 'rockChar';
-    } else if (randomNumber === 1) {
-      computerChoice = 'Paper';
-      document.getElementById('computerChoice').className = '';
-    } else if (randomNumber === 2) {
-      document.getElementById('computerChoice').className = 'scissorsChar';
-      computerChoice = 'Scissors';
-    }
+    let randomIndex = Math.floor(Math.random() * 3); 
+    computerChoice = choices[randomIndex];
     document.getElementById('computerChoice').innerHTML = ('Computer selected: ' + computerChoice);
+    document.getElementById('computerChoice').className = `${computerChoice.toLowerCase()}Char`;
     determineWinner();
   }
 
@@ -98,7 +70,9 @@ const determineWinner = () => {
         }
 
     // If a winning character was chosen from the user
-    if ((userChoice === 'Rock' && computerChoice === 'Scissors') || (userChoice === 'Paper' && computerChoice === 'Rock') || (userChoice === 'Scissors' && computerChoice === 'Paper')) {
+    if ((userChoice === 'Rock' && computerChoice === 'Scissors')
+       || (userChoice === 'Paper' && computerChoice === 'Rock')
+       || (userChoice === 'Scissors' && computerChoice === 'Paper')) {
         result = 'You win!';
         document.getElementById('winner').className = 'winnerPlayer';
         console.log(playerScore++);
@@ -106,7 +80,9 @@ const determineWinner = () => {
     
 
     // If a winning character was chosen from the computer
-    if ((userChoice === 'Rock' && computerChoice === 'Paper') || (userChoice === 'Paper' && computerChoice === 'Scissors') || (userChoice === 'Scissors' & computerChoice === 'Rock')) {
+    if ((userChoice === 'Rock' && computerChoice === 'Paper')
+       || (userChoice === 'Paper' && computerChoice === 'Scissors')
+       || (userChoice === 'Scissors' & computerChoice === 'Rock')) {
         result = 'The computer wins!';
         document.getElementById('winner').className = 'winnerComputer';
         console.log(computerScore++);
@@ -173,15 +149,15 @@ const startGame = () => {
 }
 
 const enableButtons = () => {
-  document.getElementById('rockSelect').disabled = false;
-  document.getElementById('paperSelect').disabled = false;
-  document.getElementById('scissorsSelect').disabled = false;
+  choices.forEach(choice => {
+    document.getElementById(`${choice}Select`).disabled = false;
+  });
 }
 
 const disableButtons = () => {
-  document.getElementById('rockSelect').disabled = true;
-  document.getElementById('paperSelect').disabled = true;
-  document.getElementById('scissorsSelect').disabled = true;
+  choices.forEach(choice => {
+    document.getElementById(`${choice}Select`).disabled = true;
+  })
 }
 
 disableButtons();
